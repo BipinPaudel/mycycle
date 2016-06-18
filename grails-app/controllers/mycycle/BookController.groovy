1eca2d8d8697be9276f58d091df54d4a5cd15c90
BipinPaudel <paudelbipin.bp@gmail.com>
2016-06-18 15:37:00 +0545

    Difference added, return left

--- grails-app/controllers/mycycle/BookController.groovy
+++ grails-app/controllers/mycycle/BookController.groovy
@@ -1,30 +1,52 @@
 package mycycle
 
+import org.apache.tools.ant.types.resources.selectors.InstanceOf
+
 class BookController {
     def springSecurityService
+
     def index() {
 
     }
 
-    def book(){
-        def bicycleId=params.id
-        [bicycleId:bicycleId]
+    def book() {
+        def bicycleId = params.id
+        [bicycleId: bicycleId]
     }
 
-    def save(){
-        params.status=0
-        params.paid=0
-        params.user=springSecurityService.currentUser.id
-        println(params)
-        def booking= new Book(params)
+    def save() {
+        def bookedQuantity = Integer.parseInt(params.quantity)
+        def bicycleId = params.bicycle
+        println("main  :" + Bicycle.findById(bicycleId).getRemaining())
+        def inStock = Bicycle.findById(bicycleId).remaining
 
-        if(booking.save()){
-            println("Everythings okay")
-            redirect (view: "index", controller: 'category')
-        }
-        else{
-            render "Not done "
-        }
+        if (inStock < bookedQuantity) {
+            flash.message = "Not that much bicycle in stock!"
+            redirect(controller: 'category', view: 'index')
+            return
+        } else {
 
+            params.status = 0
+            params.paid = 0
+            params.user = springSecurityService.currentUser.id
+            println(params)
+            def booking = new Book(params)
+
+            if (booking.save()) {
+//                println("Everythings okay")
+//               println("id chai : " + bicycleId.getClass())
+                def remaining = inStock - bookedQuantity
+
+//                println("remaining is :"  + remaining + " " + " type is: " + remaining.getClass()  )
+                Bicycle.executeUpdate("update Bicycle set remaining = ? where id= ?",
+                        [remaining,Long.parseLong(bicycleId)] )
+                redirect(controller: 'category', view: 'index')
+                return
+            } else {
+                render "Not done "
+            }
+        }
     }
+
+
 }
